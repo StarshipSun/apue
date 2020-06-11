1e4a74de226eeab7328d02fe8b02d9fb67a0e905
linguangsheng <lgs3063007539@gmail.com>
2020-06-12 05:50:27 +0800

    chapter03 习题2：释放临时文件描述符

--- Chapter-03/p3.2.c
+++ Chapter-03/p3.2.c
@@ -1,11 +1,24 @@
 #include "apue.h"
 #include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+struct Node {
+	int fd;
+	struct Node *next;
+};
 
 int my_dup2(int, int);
 
+void free_fd(struct Node *head);
+
 int main(void) {
     int newfd = my_dup2(STDIN_FILENO, 10);
-    printf("dup2 for stdin_fileno: %d, newfd: %d\n", STDIN_FILENO, newfd);
+	if (newfd < 0) {
+		err_sys("my_dup2 error");
+	} else {
+    	printf("my_dup2 stdin_fileno: %d, newfd: %d\n", STDIN_FILENO, newfd);
+	}
     exit(0);
 }
 
@@ -23,12 +36,39 @@ int my_dup2(int fd, int fd2) {
     }
 
     int newfd;
+	struct Node *head = (struct Node *) malloc(sizeof(struct Node));
+	struct Node *cur = head;
+
     while ((newfd = dup(fd)) != fd2)
     {
         if (newfd < 0) {
-            err_sys("dup error");
-        }
+			free_fd(head);
+            printf("dup error for %d\n", fd);
+			return -1;
+        } else {
+			printf("create fd:%d\n", newfd);
+			struct Node *temp = (struct Node *) malloc(sizeof(struct Node));
+			temp->fd = newfd;
+			temp->next = NULL;
+			cur->next = temp;
+			cur = temp;
+		}
     }
-    
+    free_fd(head);
     return newfd;
+}
+
+void free_fd(struct Node *head) {
+	struct Node *cur;
+	while (head->next != NULL) {
+		if (close(head->next->fd) == -1) {
+			printf("close fd:%d error\n", head->next->fd);
+		} else {
+			printf("close fd:%d success\n", head->next->fd);
+		}
+		cur = head->next;
+		free(head);
+		head = cur;
+	}
+	free(head);
 }
