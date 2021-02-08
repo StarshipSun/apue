7aeddf53b40cdae5b48b4386361d03bc1fd4cc37
linguangsheng <lgs3063007539@gmail.com>
2021-02-08 08:10:41 +0800

    8.9示例

---
+++ Chapter-08/8.9.2.c
@@ -0,0 +1,31 @@
+#include "apue.h"
+
+static void charatatime(char *);
+
+int main(void)
+{
+    pid_t pid;
+
+    TELL_WAIT();
+
+    if ((pid = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid == 0) {
+        WAIT_PARENT();
+        charatatime("output from child: 1111111111111111111111111111\n");
+    } else {
+        charatatime("output from parent: 222222222222222222222222222\n");
+        TELL_CHILD(pid);
+    }
+    exit(0);
+}
+
+static void charatatime(char *str)
+{
+    char *ptr;
+
+    setbuf(stdout, NULL);  // 将标准输出设置为不带缓冲
+    for (ptr = str; *ptr != 0; ptr++) {
+        putc(*ptr, stdout);  // 每次调用 putc 都会发生系统调用 write，增加竞争
+    }
+}
